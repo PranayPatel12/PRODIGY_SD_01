b6d7e3b4e44fa04bd04ac4ec59dcad67b9a443d2
Pranay <118924300+PranayPatel12@users.noreply.github.com>
2025-03-22 11:31:22 +0530

    Temperature Conversion Program

--- Task1.java
+++ Task1.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the Temperature: ");
